56e268e02c09a6164ce53ee705d29c04d5dee6c4
Saurabh Prakash <spraka10@asu.edu>
2019-02-06 18:48:59 -0700

    Task 28: added shell scripting file for switching between apps

---
+++ switchApps.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+Echo "Switching Container"
+
+docker ps
+
+cd moduleOne
+
+docker build -t moduleone .
+
+cd ..
+
+cd moduleOne
+
+docker build -t moduletwo .
+
+cd ..
+
+docker run --name one -d -p 3001:3000 moduleone:latest 
+docker run --name two -d -p 3002:3000 moduletwo:latest 
+
+docker ps
+
+
+docker stop two
+
+docker stop one
+
+
+docker start one
+
+
+
+TIMEOUT 10
+
+docker stop one
+
+
+docker start one
+
+TIMEOUT 10
+
+docker stop two
+
